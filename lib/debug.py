6fe33676db6090770118074943b7c4838afec85b
{B-Lubanga} <{Lubangabenjamin@gmail.com}>
2025-05-22 23:30:02 +0300

    Add print statements in debug.py file

--- lib/debug.py
+++ lib/debug.py
@@ -10,6 +10,10 @@ mocha = Coffee('Mocha')
 latte = Coffee('Latte')
 espresso = Coffee('Espresso')
 
-order1 = Order()
-order2 = Order()
-order3 = Order()
+order1 = Order(benjie,latte,4.0)
+order2 = Order(benjie,espresso,3.5)
+order3 = Order(aston,latte,4.5)
+
+print(benjie.coffees()) 
+print(latte.average_price())  
+print(Customer.most_aficionado(latte).name)  
